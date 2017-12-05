362719019edd67d385690dbefd9b98e80635f084
Marcus Battle <marcusbattle@marcbook.attlocal.net>
2017-12-05 02:16:09 -0500

    Removed the news notification

--- themes/bb-blueprint/header-restaurant.php
+++ themes/bb-blueprint/header-restaurant.php
@@ -8,14 +8,7 @@
 		<title><?php echo bp_get_page_title(); ?></title>
 		<?php wp_head(); ?>
 	</head>
-	<body style="padding-top: 60px;">
-		<section class="restaurant" style="background-color: #EA4137; color: #fff; position: fixed; z-index: 1000; width: 100%; top: 0;">
-			<div class="section-body has-text-centered">
-				<div class="wrapper" style="padding: 0;">
-					<p style="margin-bottom: 0; color: #fff;">NOLA is temporarily closed due to a small fire. <a href="<?php echo home_url('nola-temporarily-closed'); ?>" style="font-weight: bold; color: #fff;">Click here</a> to read more.</p>
-				</div>
-			</div>
-		</section>
+	<body>
 		<header class="restaurant is-small has-overlay" style="<?php echo ( get_theme_mod( 'homepage_header_background' ) ) ? "background-image: url('" . get_theme_mod( 'homepage_header_background' ) . "'); background-position: center; background-repeat: no-repeat; background-size: cover;" : ''; ?>">
 			<div class="section-header has-no-padding">
 				<div class="wrapper has-text-centered">
--- themes/bb-blueprint/header.php
+++ themes/bb-blueprint/header.php
@@ -8,11 +8,4 @@
 		<title><?php echo bp_get_page_title(); ?></title>
 		<?php wp_head(); ?>
 	</head>
-	<body style="padding-top: 60px;">
-		<section class="restaurant" style="background-color: #EA4137; color: #fff; position: fixed; z-index: 1000; width: 100%; top: 0;">
-			<div class="section-body has-text-centered">
-				<div class="wrapper" style="padding: 0;">
-					<p style="margin-bottom: 0; color: #fff;">NOLA is temporarily closed due to a small fire. <a href="<?php echo home_url('nola-temporarily-closed'); ?>" style="font-weight: bold; color: #fff;">Click here</a> to read more.</p>
-				</div>
-			</div>
-		</section>
+	<body>
